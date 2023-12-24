51e117ec01a8e9a031459773860d5eef4621fc01
senilm <senilmendapara.it20@scet.ac.in>
2023-12-24 12:24:53 +0530

    Basic Setup

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+
+
+const Dashboard = () => {
+  return (
+    <div>Hello from Dashboard</div>
+  )
+}
+
+export default Dashboard
+
+// import React from 'react'
+// above line has to be removed in order to get going
